9d813ce1b947f28270356d9ca7b09924d2910211
Eduardo Martins <edu.almeida.01@hotmail.com>
2024-01-23 19:50:57 -0300

    Correcting warning

--- src/br/com/alura/screenmatch/PrincipalComBuscas.java
+++ src/br/com/alura/screenmatch/PrincipalComBuscas.java
@@ -5,12 +5,19 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalComBuscas {
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner read = new Scanner(System.in);
+        System.out.println("Digite o nome do filme que está buscando:");
+        var search = read.nextLine();
+
+        String url = "https://www.omdbapi.com/?t=" + search + "&apikey=f1cb3336";
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.omdbapi.com/?t=avatar&apikey=f1cb3336"))
+                .uri(URI.create(url))
                 .build();
 
         HttpResponse<String> response = client
